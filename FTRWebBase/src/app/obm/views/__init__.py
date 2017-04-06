d5df6ae348610800877a602991fad3fb8368fb3a
BaekSeungWoo <BaekSeungWoo@DESKTOP-CPR6UII>
2017-04-06 15:46:42 +0900

    .

--- FTRWebBase/src/app/obm/views/__init__.py
+++ FTRWebBase/src/app/obm/views/__init__.py
@@ -1,28 +1,14 @@
-from app import db,ma 
 from datetime import datetime
 
-from app.obm.views.view_ob_resources import *
+from app.obm.views.endpoint_view import *
+from app.obm.views.device_view import *
+from app.obm.views.gateway_view import *
+from app.obm.views.resource_view import *
 
+'''
 __all__ = [ 
             "OB_RESOURCE_VIEW"
             , "OB_GATEWAY_VIEW"
-            , "OB_DEVICE_VIEW"
-            , "OB_ENDPOINT_VIEW"
-            , "OB_DEVTYPE_SAVE"
-            , "OB_DEVTYPE_UPDATE"
-            , "OB_DEVTYPE_DELETE" 
-            , "OB_DEVTYPE_MAP_VIEW"
-            , "OB_EPTYPE_VIEW"
-            , "OB_RESOURCE_SAVE"
-            , "OB_GATEWAY_SAVE"
-            , "OB_RESOURCE_SAVE"
-            , "OB_DEVTYPE_VIEW"
-            , "OB_ENDPOINT_SAVE"
-            , "OB_EPTYPE_SAVE"
-            , "OB_EPTYPE_UPDATE"
-            , "OB_EPTYPE_DELETE"
-           
-#             , ""
         ]
-
+'''
 
