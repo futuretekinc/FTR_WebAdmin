78e6cd9055011f1fb131b319064bd6a6eb8ec13e
BaekSeungWoo <BaekSeungWoo@DESKTOP-CPR6UII>
2017-07-07 15:57:55 +0900

    ..

---
+++ FTRWebBase/src/app/cmm/services/service.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import json
+import pandas as pd
+from app import db
+from app.cmm.models import *
+from app.obm.models import *
+from app.cmm.utils.decimal_jsonizer import fn_jsonify
+
+class SCH_CM_USER(ModelSchema): 
+    class Meta: model = CM_USER
+
+sch_cm_user_many          = SCH_CM_USER(many=True)
+sch_cm_user_single        = SCH_CM_USER(many=False)
+
+class MD_CM_USER(object):
+
+    def get_entry(self):
+        return db.session.query(CM_USER).all()
+    
+    def get_profile(self,email):
+        try:
+            stat = db.session.query(CM_USER).filter(CM_USER.email == email).one()
+            result = sch_cm_user_single.dump(stat)
+            profile = result.data
+            profile['gateway'] = self.get_cm_user_gw(email)
+            return profile
+        except Exception as e:
+            return None
+
+    def get_gw_list(self,email):
+        r = db.session.query(CM_USER_GATEWAY,OB_GATEWAY) \
+            .join(OB_GATEWAY, CM_USER_GATEWAY.gw_id == OB_GATEWAY.gw_id) \
+            .filter(CM_USER_GATEWAY.email == email).all()
+        return [ x[1] for x in r ]
+
+    def get_cm_user_gw(self,email):
+#         r = db.session.query(CM_USER_GATEWAY,OB_GATEWAY) \
+#             .join(OB_GATEWAY, CM_USER_GATEWAY.gw_id == OB_GATEWAY.gw_id) \
+#             .filter(CM_USER_GATEWAY.email == email).all()
+#         gw = [ x[1] for x in r ]
+        gw = self.get_gw_list(email)
+        result = ob_gateway_many.dump(gw)
+        for x in result.data:
+            r = u"등록완료" if x.get('register') == 'Y' else u"등록대기"
+            x.update({'register' : r })
+        return result.data        
+        
+    def save(self, email, name, passwd):
+        try:
+            user = CM_USER(email, name, passwd)
+            db.session.add(user);
+            db.session.commit()
+            return True,'success'
+        except Exception as e:
+            db.session.rollback()
+            return False, str(e)
+    
+    def save_user_gateway(self, email, gw_id):
+        try:
+            cug = CM_USER_GATEWAY()
+            cug.email = email
+            cug.gw_id = gw_id
+            db.session.add(cug)
+            db.session.commit()
+            return True, 'success'
+        except Exception as e:
+            db.session.rollback()
+            return False, str(e)
+
+    def update(self,email, name, passwd):
+        try:
+            user = db.session.query(CM_USER).filter(CM_USER.email == email).one()
+            user.name = name
+            user.passwd = passwd
+            db.session.commit()
+            return True,'success'
+        except Exception as e:
+            db.session.rollback()
+            return False, str(e)
+        
+    def delete(self, email):
+        try:
+            user = db.session.query(CM_USER).filter(CM_USER.email == email).one()
+            db.session.delete(user)
+            db.session.commit()
+            return True,'success'
+        except Exception as e:
+            db.session.rollback()
+            return False, str(e)
+ 
+ 
+class MD_EP_TYPE(object): 
+    def get_entry(self,gw_id):
+        try:
+            query = self.get_query(gw_id)
+            df = pd.read_sql_query(query,con=db.engine)
+            df = df.fillna('')
+            data = json.loads(df.to_json(orient='records'))
+            return fn_jsonify({ 'data' : data })
+        except Exception as e:
+            return fn_jsonify({'data' : []})
+    
+    def get_query(self,gw_id):
+        return '''
+SELECT 
+    d.comd_code code, d.comd_cdnm name
+FROM
+    OB_GATEWAY_MAP a
+        INNER JOIN
+    OB_DEVICE_MAP b ON a.dev_id = b.dev_id
+        INNER JOIN
+    OB_ENDPOINT c ON b.ep_id = c.ep_id
+        INNER JOIN
+    CM_CODED d ON c.ep_type = d.comd_code
+        AND d.use_yn = 'Y'
+WHERE
+    a.gw_id = '{}'
+GROUP BY c.ep_type        
+        '''.format(gw_id)
+        
+
+class MD_TIME_TYPE():        
+    def get_choice_entry(self):
+        rows = db.session.query(CM_CODED).filter(CM_CODED.comm_code == 'TIME_TYPE').all()
+        buf = []
+        for row in rows:
+            buf.append((row.comd_code, row.comd_cdnm))
+        return buf
+        
+        
+if __name__ == '__main__x':
+    r = db.session.query(CM_USER_GATEWAY,OB_GATEWAY).join(OB_GATEWAY, CM_USER_GATEWAY.gw_id == OB_GATEWAY.gw_id).filter(CM_USER_GATEWAY.email == '1a@aaa.com').all()
+    gw = []
+    for x in r:
+        gw.append(x[1])
+    result = ob_gateway_many.dump(gw)
+    print(result.data)
+if __name__ == '__main__xxx':
+    service = MD_EP_TYPE()
+    r = service.get_entry('FTM20170601012345678901234567894')
+    print(r)
+
+
+     
---
+++ FTRWebBase/src/app/rtm/services/chart_service.py
@@ -0,0 +1,126 @@
+# -*- coding : utf-8 -*-
+import pandas as pd
+from app import app, db
+from app.obm.models import *
+from app.cmm.utils.decimal_jsonizer import fn_jsonify
+import json
+from app.cmm.models import CM_USER_GATEWAY
+from app.obm.models.ob_resources import OB_GATEWAY
+
+class MD_HIGHCHARTS():
+ 
+    def get_query(self, ep_day, ep_id, email):
+        return '''
+SELECT 
+    (select gw_name from OB_GATEWAY where gw_id = d.gw_id) gw_name
+    , (SELECT ep_name FROM OB_ENDPOINT WHERE ep_id = d.ep_id) ep_name
+    , (select comd_cdnm from CM_CODED where comd_code = d.ep_type) ep_type
+    , if(ISNULL(d.ep_data), null, d.ep_data*1) ep_data
+    , unix_timestamp(str_to_date(concat(d.ep_day, d.ep_time),'%%Y%%m%%d%%H%%i')) ep_unix    
+    , d.ep_day
+    , d.ep_time
+FROM 
+CM_USER_GATEWAY a 
+INNER JOIN OB_GATEWAY_MAP b ON a.gw_id = b.gw_id 
+INNER JOIN OB_DEVICE_MAP c ON b.dev_id = c.dev_id 
+INNER JOIN RM_GET_DATA d ON c.ep_id = d.ep_id and a.gw_id = d.gw_id
+AND d.ep_day = '{0}' 
+WHERE 
+    d.ep_id = '{1}'
+    AND a.email = '{2}'
+    AND d.ep_data is not null
+    order by ep_name, ep_time desc
+        '''.format( ep_day, ep_id, email )
+        
+    def get_chart_query(self,time_type, ep_day, ep_id, email):
+        sub_query = self.get_query(ep_day, ep_id, email)
+        return '''
+            select t1.time_key times, t2.ep_data, t2.ep_unix
+            from 
+            CM_TIME t1 left outer join (
+             {0}
+            ) t2 on t1.time_key = t2.ep_time where  t1.time_type = '{1}'
+        '''.format(sub_query, time_type)
+
+    def get_type_query(self, email, ep_type, gw_id):
+        return '''
+select 
+    b.gw_id
+    , (select gw_name from OB_GATEWAY where gw_id = b.gw_id) gw_name    
+    , d.ep_id
+    , d.ep_type
+    , (select comd_cdnm from CM_CODED where comd_code = d.ep_type) ep_type_kor
+    , d.ep_name
+from 
+    CM_USER_GATEWAY a 
+    inner join OB_GATEWAY_MAP b on a.gw_id = b.gw_id 
+    inner join OB_DEVICE_MAP c on b.dev_id = c.dev_id 
+    inner join OB_ENDPOINT d on c.ep_id = d.ep_id
+    where 
+        a.email = '{0}' 
+        and d.ep_type='{1}'
+        and b.gw_id = '{2}'        
+        '''.format(email, ep_type, gw_id)
+
+    def get_type_entry(self,time_type, ep_day, email, ep_type,gw_id):
+        try:
+            query = self.get_type_query(email, ep_type,gw_id)
+            df = pd.read_sql_query(query, con=db.engine)
+            records = df.to_dict(orient='records')
+            for record in records:
+                ep_id = record.get('ep_id')
+                ca = self.get_chart_entry(time_type, ep_day, email, ep_type,ep_id)
+                record.update(ca)
+            return records
+        except Exception as e:
+            print("ERROR-->", str(e))
+            return {}
+
+    def get_chart_entry(self,time_type, ep_day, email, ep_type,ep_id): 
+        query = self.get_query(ep_day, ep_id, email)
+        ct_query= self.get_chart_query(time_type, ep_day, ep_id, email)
+        df = pd.read_sql_query(query,con=db.engine)
+        cdf = pd.read_sql_query(ct_query,con=db.engine)
+        cdf = cdf.where((pd.notnull(cdf)),None)
+        json_str = df.to_json(orient='records')
+        return {
+            'table_data' : json.loads(json_str) 
+            , 'chart_data' :  {'times' : cdf.times.tolist(),'unix' : cdf.ep_unix.tolist(), 'values' : cdf.ep_data.tolist() }
+        }        
+    
+if __name__ == '__main__':
+    
+    service = MD_HIGHCHARTS()
+    d = service.get_type_entry('MIN_5', '20170630','1a@aaa.com','ep_s_temperature','FTM20170601012345678901234567894')
+    
+    
+    from pprint import pprint
+    pprint(d)
+        
+    print(fn_jsonify({'data' : d}))
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
